4db7686fdbef3d3adc26836f29e15e721bfab0c6
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-30 18:38:27 +0900

    Link to ts_ideal_layout

--- src/views/My01View.vue
+++ src/views/My01View.vue
@@ -10,9 +10,13 @@ import My01 from '@/components/My01.vue'
     <My01 msg="lalala"  />
   </main>
   <footer class="mt-5 mb-5 container">
-    <p><a href="https://oha-ohashi.github.io/2022_1/a_ortho/ortho_heat.html">
-      Visit "オルソリひーと | Ortho Heat"
+    <p>Wanna create a heatmap? Visit <a href="https://oha-ohashi.github.io/2022_1/a_ortho/ortho_heat.html">
+      "オルソリひーと | Ortho Heat"
     </a></p>
+    <p>Wanna compare layouts or custermise the evaluation function? Try
+      <a href="https://github.com/oha-Ohashi/ts_ideal_layout/tree/main">
+        CLI version
+    </a> (node.js required).</p>
     <p><a href="https://github.com/oha-Ohashi/ortho_search">
       GitHub Repository
     </a></p>
